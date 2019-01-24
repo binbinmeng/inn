ad4d95215eb5f8f569c6c0ae05d4150498b0e041
medialab <fdsafdas>
2019-01-24 17:59:08 +0800

    version 1: simple implementation, support lenet, add layers manually

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ echo nvcc
 nvcc -o lenet main.cpp caffe.pb.a int8_conv_layer.cpp int8_convba_layer.cpp int8_data_layer.cpp int8_pooling_layer.cpp int8_relu_layer.cpp int8_layer.cpp int8_net.cpp int8_fc_layer.cpp int8_fc_layer.cu -lcudnn -lcublas -std=c++11 `pkg-config --cflags --libs opencv` -lprotobuf -lpthread -lglog -lgflags
 
 echo testing
-CUDA_VISIBLE_DEVICES=3 ./lenet
+./lenet
 
 
 
